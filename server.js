840b56a491fe4be02d256a5f3f2c47690530b81d
rroyalty <rroyalty@gmail.com>
2021-03-06 13:16:23 -0500

    why do I need to commit twice sometimes?

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT =  process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 
