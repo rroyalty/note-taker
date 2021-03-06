833cd7cf9bf3213295ddb72ab6643a495fd5f124
rroyalty <rroyalty@gmail.com>
2021-03-06 13:16:12 -0500

    express is hard

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,17 +11,8 @@ router.get("/notes", (req, res) => {
 module.exports = router;
 
 // // ROUTING
-
-// module.exports = (app) => {
 //   app.get('/api/tables', (req, res) => res.json(tableData));
 
-//   // API POST Requests
-//   // Below code handles when a user submits a form and thus submits data to the server.
-//   // In each of the below cases, when a user submits form data (a JSON object)
-//   // ...the JSON is pushed to the appropriate JavaScript array
-//   // (ex. User fills out a reservation request... this data is then sent to the server...
-//   // Then the server saves the data to the tableData array)
-//   // ---------------------------------------------------------------------------
 
 //   app.post('/api/tables', (req, res) => {
 //     if (tableData.length < 5) {
