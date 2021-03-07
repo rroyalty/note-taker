5a39b549c0ab3310eea9a5617fc58b2e5a21e7aa
rroyalty <rroyalty@gmail.com>
2021-03-07 09:34:31 -0500

    added delete functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,7 @@ const { fstat } = require("fs");
 
 router.get("/notes", (req, res) => {
     res.json(dbData);
-    });
+});
 
 router.post("/notes", (req, res) => {
     let newNote = {
@@ -17,6 +17,14 @@ router.post("/notes", (req, res) => {
     };
     dbData.push(newNote);
     res.json(dbData);
+});
+
+router.delete("/notes/:id", (req, res) => {
+    let note = dbData.findIndex( (ele) => {
+        return ele.id == req.params.id
     });
+    dbData.splice(note,1);
+    res.json(dbData);
+});
 
 module.exports = router;
